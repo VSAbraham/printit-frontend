c6fbc3fe863642ba4b6a0d8af8f1bc4f0028624f
VSAbraham <93576286+VSAbraham@users.noreply.github.com>
2025-02-19 17:13:37 +0530

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/prinit-frontend/",
+  base: "/printit-frontend/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
