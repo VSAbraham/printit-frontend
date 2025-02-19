762345b9a8723a8db03deb324d468a656ca0e169
VSAbraham <93576286+VSAbraham@users.noreply.github.com>
2025-02-19 17:26:27 +0530

    duplication removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import OrderPage from './pages/OrderPage';
 import PreviewPage from './pages/PreviewPage';
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/order" element={<OrderPage />} />
         <Route path="/preview" element={<PreviewPage />} />
       </Routes>
-    </Router>
   );
 }
 
